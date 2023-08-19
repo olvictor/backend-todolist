66237caecd72797c3c7ecf55a5ab87d0feffabd8
VictorOlv <victorolvdev@gmail.com>
2023-08-18 22:52:36 -0300

    criacao das rotas

---
+++ controladores/todos.js
@@ -0,0 +1,5 @@
+
+
+const getTodos = (req,res) =>{
+
+}
---
+++ src/roteador.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const rotas = express();
+
+rotas.get('/todos')
+rotas.post('/todos')
+
+
+
+
+module.exports = rotas
